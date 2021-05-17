8ede7ec9a9fb1c61c52acf4b8431e4cdf5b710df
Jerozan Jeyendrarasa <me@jerojeyen.com>
2021-05-17 15:19:46 +0200

    Init SwiftUI proj

---
+++ BlurAndVibrancy/BlurAndVibrancyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlurAndVibrancyApp.swift
+//  BlurAndVibrancy
+//
+//  Created by Jerozan Jeyendrarasa on 17/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct BlurAndVibrancyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
